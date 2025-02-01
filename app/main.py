851174e0402bc01ef7e740f25932b5cba3ce5aae
AyyyStew <alexsteward98@gmail.com>
2025-02-01 12:41:50 -0600

    created simple api for downloading and viewing data

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import json
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def home():
-    return {"msg": "Hello World"}
+async def home(request: Request):
+    return templates.TemplateResponse(request=request, name="home.html", context={})
+
+
+@app.get("/view")
+async def view(request: Request):
+    with open("data/sample.json", "r") as file:
+        data = json.load(file)
+    return data
+
+
+@app.get("/download")
+async def download(request: Request):
+    return FileResponse(
+        path="./data/sample.json", filename="sample.json", media_type="application/json"
+    )
--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,18 @@ from .main import app
 client = TestClient(app)
 
 
-def test_home_works():
+# Note I know these tests are not good tests, but they do show me what endpoints break when changes are made.
+# Tests need to added to mock and verify data. For this pet project though, I do not care to.
+def test_home_200():
     response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_view_200():
+    response = client.get("/view")
+    assert response.status_code == 200
+
+
+def test_download_200():
+    response = client.get("/download")
     assert response.status_code == 200
